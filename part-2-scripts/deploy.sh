e530b31935e028a0cf9773858971bdcdd490d4d4
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-08 17:07:55 +0530

    version change

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.0/bin
 
 cd Week-9
  git pull origin master
  cd server
+ npm install
  pm2 kill
  pm2 start index.js
 ~                     
